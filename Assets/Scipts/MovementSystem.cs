c270c9b52752585a87784c0f2e4928d757e1f9fb
Windocs <mr.potatoe193@gmail.com>
2022-02-25 12:22:00 +0100

    Implemented Sprinting
    GetKeyCode.LeftShift joa

--- Assets/Scipts/MovementSystem.cs
+++ Assets/Scipts/MovementSystem.cs
@@ -19,7 +19,7 @@ public class MovementSystem : MonoBehaviour {
 	void Start () 
 	{	
 		cc = GetComponent<CharacterController>();
-		moveSpeed = 10f;
+		moveSpeed = 5f;
 		jumpHeight = 1f;	// jumpHieght 1 und gravity -1 passen am besten
 		gravity = -1f;
 		jumping = false;
@@ -34,6 +34,12 @@ public class MovementSystem : MonoBehaviour {
 		{
 			jumping = true;
 		}
+
+		if (Input.GetKeyDown(KeyCode.LeftShift))
+            StartSprint();
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+            StopSprint();
+
 	}
 
 	private void FixedUpdate() // Es ist besser alle Bewegungen im FixedUpdate zu machen, da es nicht jeden Frame passiert => mehr Zeit
@@ -57,4 +63,16 @@ public class MovementSystem : MonoBehaviour {
 		cc.Move((transform.forward * verticalAxis + transform.right * horizontalAxis).normalized * moveSpeed * Time.deltaTime);
 		cc.Move(Vector3.up * upwardVelocity);
     }
+
+	private void StartSprint()
+    {
+        moveSpeed = moveSpeed * 2;
+    }
+
+    private void StopSprint()
+    {
+        moveSpeed = moveSpeed / 2;
+    }
+
+
 }
